67f102dab1f1cce2d8067a92c9b1e8a9c2e20a3f
unknown <trex33160@gmail.com>
2021-03-14 16:18:15 +0545

    Updates

---
+++ precache-manifest.3f30f03fed34a10d40dab1ac3eed131c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab634f33da9e0a5e668b21212339ae3",
+    "url": "/gh-page-test/index.html"
+  },
+  {
+    "revision": "0e5209f74d5b14d50184",
+    "url": "/gh-page-test/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "6a0a0c5504f52ef2be34",
+    "url": "/gh-page-test/static/js/2.81cf64c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-page-test/static/js/2.81cf64c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e5209f74d5b14d50184",
+    "url": "/gh-page-test/static/js/main.2b16ce4f.chunk.js"
+  },
+  {
+    "revision": "dc46f0100492838760be",
+    "url": "/gh-page-test/static/js/runtime-main.f3b9c8da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-page-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
